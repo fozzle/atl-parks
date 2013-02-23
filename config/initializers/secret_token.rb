8958d72416bac5a16f42f3bdb8339c28f31aa0c4
Kyle Petrovich <kylepetrovich@gmail.com>
2013-02-22 20:43:59 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtlParks::Application.config.secret_token = '8162d2051c61778709d786f8fef1f3db6c8dd5535dc25b155802bb16ffef45ee863275bfcb6acc0fb81d1bcce9f26f43c7f2712b274a357b9ed763c3fbb91872'
