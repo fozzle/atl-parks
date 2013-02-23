81a92003766bebb77053b12eff7c699e80770798
Kyle Petrovich <kylepetrovich@gmail.com>
2013-02-23 01:47:30 -0500

    some query options

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -2,7 +2,42 @@ class ParksController < ApplicationController
   # GET /parks
   # GET /parks.json
   def index
-    @parks = Park.all
+
+    @parks = Park.scoped
+    @parks = @parks.where("pavilions >= ?", params[:pavilions]) if params[:pavilions].present?
+    @parks = @parks.where("playgrounds >= ?", params[:playgrounds]) if params[:playgrounds].present?
+    @parks = @parks.where("picnic_shelters >= ?", params[:picnic_tables]) if params[:picnic_tables].present?
+    @parks = @parks.where("grills >= ?", params[:grills]) if params[:grills].present?
+    @parks = @parks.where("restrooms >= ?", params[:restrooms]) if params[:restrooms].present?
+    @parks = @parks.where("parking_spaces >= ?", params[:parking_spaces]) if params[:parking_spaces].present?
+    @parks = @parks.where("ball_fields >= ?", params[:ball_fields]) if params[:ball_fields].present?
+    @parks = @parks.where("soccer_fields >= ?", params[:soccer_fields]) if params[:soccer_fields].present?
+    @parks = @parks.where("tennis_courts >= ?", params[:tennis_courts]) if params[:tennis_courts].present?
+    @parks = @parks.where("basketball_courts >= ?", params[:basketball_courts]) if params[:basketball_courts].present?
+    @parks = @parks.where("volleyball_courts >= ?", params[:volleyball_courts]) if params[:basketball_courts].present?
+    @parks = @parks.where(pool: true) if params[:pool].present?
+    # conditions << [ "classification = ?", params[:classification] ] if params[:classification].present?
+    # conditions << [ "pavilions > ?", params[:pavilions].to_i ] if params[:pavilions].present?
+    # @parks = Park.all(:conditions => conditions)
+
+
+    # t.string :name
+    #   t.string :classification
+    #   t.string :address
+    #   t.decimal :gis_ac
+    #   t.integer :zip_code
+    #   t.integer :tennis_courts
+    #   t.integer :basketball_courts
+    #   t.integer :volleyball_courts
+    #   t.boolean :pool
+    #   t.boolean :gym
+    #   t.boolean :recreation_center
+    #   t.boolean :covbb
+    #   t.boolean :dog_park
+    #   t.boolean :track
+    #   t.boolean :nat
+    #   t.boolean :golf
+    #   t.decimal :area
 
     respond_to do |format|
       format.html # index.html.erb
