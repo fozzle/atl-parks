3930396b54e8717fc13d12bd99df2c333c30dde4
Kyle Petrovich <kylepetrovich@gmail.com>
2013-02-23 01:54:38 -0500

    finished 'api' and commented out inappropriate methods

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -15,29 +15,15 @@ class ParksController < ApplicationController
     @parks = @parks.where("tennis_courts >= ?", params[:tennis_courts]) if params[:tennis_courts].present?
     @parks = @parks.where("basketball_courts >= ?", params[:basketball_courts]) if params[:basketball_courts].present?
     @parks = @parks.where("volleyball_courts >= ?", params[:volleyball_courts]) if params[:basketball_courts].present?
-    @parks = @parks.where(pool: true) if params[:pool].present?
-    # conditions << [ "classification = ?", params[:classification] ] if params[:classification].present?
-    # conditions << [ "pavilions > ?", params[:pavilions].to_i ] if params[:pavilions].present?
-    # @parks = Park.all(:conditions => conditions)
-
-
-    # t.string :name
-    #   t.string :classification
-    #   t.string :address
-    #   t.decimal :gis_ac
-    #   t.integer :zip_code
-    #   t.integer :tennis_courts
-    #   t.integer :basketball_courts
-    #   t.integer :volleyball_courts
-    #   t.boolean :pool
-    #   t.boolean :gym
-    #   t.boolean :recreation_center
-    #   t.boolean :covbb
-    #   t.boolean :dog_park
-    #   t.boolean :track
-    #   t.boolean :nat
-    #   t.boolean :golf
-    #   t.decimal :area
+    @parks = @parks.where(pool: true) if params[:pool] == 1
+    @parks = @parks.where(gym: true) if params[:gym] == 1
+    @parks = @parks.where(recreation_center: true) if params[:recreation_center] == 1
+    @parks = @parks.where(covbb: true) if params[:covbb] == 1
+    @parks = @parks.where(dog_park: true) if params[:dog_park] == 1
+    @parks = @parks.where(track: true) if params[:track] == 1
+    @parks = @parks.where(nat: true) if params[:nat] == 1
+    @parks = @parks.where(golf: true) if params[:golf] == 1
+    @parks = @parks.where("area >= ?", params[:area]) if params[:area]
 
     respond_to do |format|
       format.html # index.html.erb
@@ -56,63 +42,63 @@ class ParksController < ApplicationController
     end
   end
 
-  # GET /parks/new
-  # GET /parks/new.json
-  def new
-    @park = Park.new
+  # # GET /parks/new
+  # # GET /parks/new.json
+  # def new
+  #   @park = Park.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @park }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @park }
+  #   end
+  # end
 
-  # GET /parks/1/edit
-  def edit
-    @park = Park.find(params[:id])
-  end
+  # # GET /parks/1/edit
+  # def edit
+  #   @park = Park.find(params[:id])
+  # end
 
   # POST /parks
   # POST /parks.json
-  def create
-    @park = Park.new(params[:park])
+  # def create
+  #   @park = Park.new(params[:park])
 
-    respond_to do |format|
-      if @park.save
-        format.html { redirect_to @park, notice: 'Park was successfully created.' }
-        format.json { render json: @park, status: :created, location: @park }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @park.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @park.save
+  #       format.html { redirect_to @park, notice: 'Park was successfully created.' }
+  #       format.json { render json: @park, status: :created, location: @park }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @park.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # PUT /parks/1
-  # PUT /parks/1.json
-  def update
-    @park = Park.find(params[:id])
+  # # PUT /parks/1
+  # # PUT /parks/1.json
+  # def update
+  #   @park = Park.find(params[:id])
 
-    respond_to do |format|
-      if @park.update_attributes(params[:park])
-        format.html { redirect_to @park, notice: 'Park was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @park.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @park.update_attributes(params[:park])
+  #       format.html { redirect_to @park, notice: 'Park was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @park.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /parks/1
   # DELETE /parks/1.json
-  def destroy
-    @park = Park.find(params[:id])
-    @park.destroy
+  # def destroy
+  #   @park = Park.find(params[:id])
+  #   @park.destroy
 
-    respond_to do |format|
-      format.html { redirect_to parks_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to parks_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 end
